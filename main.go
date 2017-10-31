5056b2b2ec01b4536b5179cd6f0ebffb5ad9a0e0
Aaron Lindsay <aaron@aclindsay.com>
2017-10-30 21:06:27 -0400

    Fixup build error from typo
    This was introduced in 3869f3a3b2ca76238d13bae734fcc18af6700360

--- main.go
+++ main.go
@@ -66,7 +66,7 @@ func staticHandler(w http.ResponseWriter, r *http.Request, basedir string) {
 }
 
 func main() {
-	dsn := db.GetDSN(cfg.MoneyGo.DbType, cfg.MoneyGo.DSN)
+	dsn := db.GetDSN(cfg.MoneyGo.DBType, cfg.MoneyGo.DSN)
 	database, err := sql.Open(cfg.MoneyGo.DBType.String(), dsn)
 	if err != nil {
 		log.Fatal(err)
